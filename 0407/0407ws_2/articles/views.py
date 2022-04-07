717b93f27270b2540b7fc016af3a333b5fc7aa1a
kevin123488 <kevin123488@naver.com>
2022-04-07 22:46:09 +0900

    0407HW

--- 0407/0407ws_2/articles/views.py
+++ 0407/0407ws_2/articles/views.py
@@ -23,3 +23,35 @@ def create(request): # 생성
         'form': form,
     }
     return render(request, 'articles/create.html', context)
+
+def detail(request, pk): # 얘는 조회만 하면 되기 때문에, render 해주자
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article':article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    # POST 요청일 때
+    if request.method == 'POST':
+        article.delete()
+        return redirect('articles:index')
+    else:
+        return redirect('articles:detail', article.pk)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    # POST 요청일 때
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    context = {
+        'form':form,
+        'article':article,
+    }
+    return render(request, 'articles/update.html', context)

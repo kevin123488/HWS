15de167e9e119730e6a368764a0bbfac94a85946
kevin123488 <kevin123488@naver.com>
2022-05-10 10:29:38 +0900

    0510pr

---
+++ 0510/practice/src/components/AppParent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <input type="text"
+      v-model="parentInputData"
+      @keyup.enter="parentSend">
+  </div>
+</template>
+
+<script>
+export default {
+    name:'AppParent',
+    data: function() {
+        return {
+            parentInputData: '',
+        }
+    },
+    methods: {
+        parentSend: function() {
+            this.$emit(this.parentInputData)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
